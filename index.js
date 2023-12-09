074ababa9eaece06b1e670a1388ed5f0d7b56ed9
siddharthvj23 <vijaysiddharth23@gmail.com>
2023-12-09 17:33:54 +0530

    updated javascript

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let addBtn = document.querySelector('.add-btn');
-let modalCont = document.querySelector('.modal-box')
+let modalCont = document.querySelector('.modal-cont')
+let allPriorityColors = document.querySelectorAll('.priority-color')
+let textAreaCont = document.querySelector('.textArea-cont')
+let maincont = document.querySelector('.main-cont')
 let addTaskflag = false
+let modalPriorityColor = 'lightpink'
 
 addBtn.addEventListener('click',(event)=>{
     addTaskflag = !addTaskflag;
@@ -13,3 +17,40 @@ addBtn.addEventListener('click',(event)=>{
 
 })
 
+//selecting all ticket color
+allPriorityColors.forEach(colorEle =>{
+    colorEle.addEventListener('click',(event)=>{
+        //remove active class from all divs
+        allPriorityColors.forEach(priorityColor =>{
+            priorityColor.classList.remove('active')
+        })
+        //add active classs on div that is clicked
+        colorEle.classList.add('active')
+        modalPriorityColor = colorEle.classList[0]
+    })
+})
+
+//ticket creation
+modalCont.addEventListener('keydown',(event)=>{
+    let keyPressed = event.key
+    if (keyPressed==='Shift'){
+        //ticket create
+        let ticketDesc = textAreaCont.value
+        let ticketId = shortid()
+        createTicket(modalPriorityColor,ticketId,ticketDesc)
+        //close modal
+        modalCont.style.display = 'none'
+        addTaskflag = !addTaskflag
+        //clear textarea
+        textAreaCont.value=''
+    }
+})
+
+function createTicket (ticketColor,ticketId,ticketDesc) {
+
+    let ticketCont = document.createElement('div')
+
+    ticketCont.classList.add('ticket-cont');
+
+
+    maincont.appendChild(ticketCont)}
